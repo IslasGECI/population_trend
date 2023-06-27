c182639671c5496734b5233947bfe9756510b340
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-27 18:39:08 +0000

    🛑🧪 Fail tests

--- tests/test_filter_data.py
+++ tests/test_filter_data.py
@@ -1,10 +1,17 @@
-from population_trend import filter_by_species
+from population_trend import filter_by_species, filter_by_species_and_island
 import pandas as pd
 
 
+data = pd.read_csv("tests/data/dcco_laal_gumu_burrows_data.csv")
+species = "Laysan Albatross"
+
+
+def test_filter_by_species_and_island():
+    island = "Guadalupe"
+    filter_by_species_and_island(data, species, island)
+
+
 def test_filter_by_species():
-    data = pd.read_csv("tests/data/dcco_laal_gumu_burrows_data.csv")
-    species = "Laysan Albatross"
     obtained_df = filter_by_species(data, species)
     obtained_len = len(obtained_df)
     expected_len = 21
