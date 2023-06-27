b99cbba7537e646cc2d86fb14c08dcd2e7d355c1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-27 19:04:44 +0000

    🛑🧪 Fail tests

--- tests/test_filter_data.py
+++ tests/test_filter_data.py
@@ -15,6 +15,10 @@ island = "Guadalupe"
 def test_write_burrows_by_species_and_island():
     output_path = "tests/data/laal_guadalupe.csv"
     write_burrows_by_species_and_island(data_path, species, island, output_path)
+    obtained_csv = pd.read_csv(output_path)
+    obtained_columns = len(obtained_csv.columns)
+    expected_columns = 12
+    assert obtained_columns == expected_columns
 
 
 def test_filter_by_species_and_island():
