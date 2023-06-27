c15a7532e2b7360efed4eebd1feb4f8238557583
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-27 18:40:10 +0000

    ✅ Pass tests

--- population_trend/filter_data.py
+++ population_trend/filter_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 
+def filter_by_species_and_island(data: pd.DataFrame, species: str, island: str):
+    pass
+
+
 def filter_by_species(data: pd.DataFrame, species: str):
     return data[data.Nombre_en_ingles == species]
