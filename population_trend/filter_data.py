65a23468df228595ce88618447a586470611b59d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-27 19:02:46 +0000

    ✅ Pass tests

--- population_trend/filter_data.py
+++ population_trend/filter_data.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 
+def write_burrows_by_species_and_island(data_path, species, island, output_path):
+    data = pd.read_csv(data_path)
+    filtered = filter_by_species_and_island(data, species, island)
+
+
 def filter_by_species_and_island(data: pd.DataFrame, species: str, island: str):
     filtered_by_species = filter_by_species(data, species)
     return filter_data_by_islet(filtered_by_species, island)
