b54cee1535b5e5e70f5a2ca6e3ad0a0f355c7dc9
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-06-30 14:13:06 -0700

    🎨 Format with black

--- population_trend/population_growth_model.py
+++ population_trend/population_growth_model.py
@@ -27,7 +27,9 @@ class Population_Trend_Model:
         self.time_to_model = np.linspace(
             self.ticks_positions.min(), self.ticks_positions.max(), 100
         )
-        self.initial_population = lambda_calculator(fit_data["Temporada"], fit_data[interest_variable])[1]
+        self.initial_population = lambda_calculator(
+            fit_data["Temporada"], fit_data[interest_variable]
+        )[1]
 
     @property
     def model_min(self):
