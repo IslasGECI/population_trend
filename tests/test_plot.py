d176f05bc98b9bbc39780a214503d6ca5dcb9709
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-07 17:31:29 +0000

    ♻️  Refactor

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -3,7 +3,11 @@ from unittest.mock import Mock
 import pandas as pd
 import numpy as np
 
-from population_trend import Population_Trend_Model, Plotter_Population_Trend_Model
+from population_trend import (
+    Population_Trend_Model,
+    Plotter_Population_Trend_Model,
+    xxPlotter_Population_Trend_Model,
+)
 
 
 @pytest.mark.mpl_image_compare
@@ -11,6 +15,6 @@ def tests_Plotter_Population_Trend_Model():
     model = Mock(spec=Population_Trend_Model)
     model.med_model = np.linspace(1, 3, 100)
     dataframe = pd.DataFrame({"Isla": [1, 3], "Temporada": [2020, 2021]})
-    Plotter = Plotter_Population_Trend_Model(dataframe)
+    Plotter = xxPlotter_Population_Trend_Model(dataframe, model)
     Plotter.set_labels()
     return Plotter.plot_model(model)
