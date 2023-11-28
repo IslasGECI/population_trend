6a7be99ae82350c57fe3940fc9a78aad03ccbe08
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-28 19:35:14 +0000

    🛑🧪 Fail tests

--- tests/test_regional_lambdas.py
+++ tests/test_regional_lambdas.py
@@ -5,6 +5,10 @@ laal_path = "tests/data/laal_intervals.json"
 with open(laal_path) as json_file:
     laal = json.load(json_file)
 
+gumu_path = "tests/data/gumu_guadalupe_boostrap_intervals.json"
+with open(gumu_path) as json_file:
+    gumu = json.load(json_file)
+
 
 def test_read_distribution():
     distribution = read_distribution(laal)
@@ -12,5 +16,10 @@ def test_read_distribution():
     expected_first_element = 1.2130418011696964
     assert obtained_first_element == expected_first_element
     expected_n = 92
+    obtained_n = len(distribution)
+    assert obtained_n == expected_n
+
+    distribution = read_distribution(gumu)
+    expected_n = 2000
     obtained_n = len(distribution)
     assert obtained_n == expected_n
