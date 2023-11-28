e225b51fbc2b4c32e57ace3af5df46c399bb156e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-28 19:32:42 +0000

    ✅ Pass tests

--- population_trend/regional_lambdas.py
+++ population_trend/regional_lambdas.py
@@ -1,3 +1,4 @@
 def read_distribution(json_dict):
     completed_distribution = json_dict["bootstrap_intermediate_distribution"]
-    return completed_distribution[0]
+    lambdas_distribution = [sample[0] for sample in completed_distribution]
+    return lambdas_distribution
