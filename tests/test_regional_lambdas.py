ba0c049b6d9a28ad0b3dee77f6dbca75e2c5826f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-28 19:11:37 +0000

    🛑🧪 Fail tests

--- tests/test_regional_lambdas.py
+++ tests/test_regional_lambdas.py
@@ -1,5 +1,10 @@
 import json
+from population_trend import read_distribution
 
 laal_path = "tests/data/laal_intervals.json"
 with open(laal_path) as json_file:
     laal = json.load(json_file)
+
+
+def test_read_distribution():
+    distribution = read_distribution(laal)
