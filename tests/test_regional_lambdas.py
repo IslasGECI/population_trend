efc18a9b6f2e87b710b3753a302f7b44b5855383
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-29 21:23:14 +0000

    🛑🧪 Fail tests

--- tests/test_regional_lambdas.py
+++ tests/test_regional_lambdas.py
@@ -20,7 +20,10 @@ with open(gumu_path) as json_file:
 
 def test_Island_Bootstrap_Distribution_Concatenator():
     paths = f"{laal_path}, {gumu_path}"
-    Island_Bootstrap_Distribution_Concatenator(paths)
+    concatenator = Island_Bootstrap_Distribution_Concatenator(paths)
+    obatined_paths = concatenator.split_paths()
+    expected_paths = [laal_path, gumu_path]
+    assert obatined_paths == expected_paths
 
 
 def test_read_distribution():
