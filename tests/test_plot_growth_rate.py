3df1af3ec0a30b909ece383e8996482efb27b236
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-07 22:47:21 +0000

    ♻️  Refactor

--- tests/test_plot_growth_rate.py
+++ tests/test_plot_growth_rate.py
@@ -9,17 +9,19 @@ def test_plotter_growth_rate():
     lambdas_pacific = {"intervals": [1.9, 3, 7.5]}
     plotter = Plotter_Growth_Rate(lambdas_california, lambdas_pacific)
     obtained = plotter.plot_error_bars()
+    assert isinstance(obtained, plt.axes._axes.Axes)
 
     x_positions = plotter.error_bar_container.lines[0].get_data()[0]
     y_positions = plotter.error_bar_container.lines[0].get_data()[1]
     np.testing.assert_array_equal(x_positions, [1, 2])
     np.testing.assert_array_equal(y_positions, [2, 3])
+
     obtained_segments = plotter.error_bar_container.lines[2][0].properties()["segments"]
     assert obtained_segments[0][0][1] == lambdas_california["intervals"][0]
     assert obtained_segments[0][1][1] == lambdas_california["intervals"][2]
     assert obtained_segments[1][0][1] == lambdas_pacific["intervals"][0]
     assert obtained_segments[1][1][1] == lambdas_pacific["intervals"][2]
-    assert isinstance(obtained, plt.axes._axes.Axes)
+
     obtained_y_label = obtained.get_ylabel()
     expected_y_label = "Growth Rate"
     assert obtained_y_label == expected_y_label
@@ -36,9 +38,9 @@ def test_plotter_growth_rate():
     height = 1
     assert segment[0][1] == height
     assert segment[1][1] == height
+
     linestyle = obtained_h_line_in_1.get_linestyle()[0][1]
     assert pytest.approx(linestyle) == [5.55, 2.4]
-    print(linestyle)
+
     color_line = obtained_h_line_in_1.get_color()[0]
-    print(color_line)
     np.testing.assert_almost_equal(color_line, [0, 0.5019607, 0, 1])
