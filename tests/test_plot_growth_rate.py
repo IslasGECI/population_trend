51b1ee2988c2923bbc018b40c075f7e7c269807a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-07 21:51:45 +0000

    ♻️  Refactor

--- tests/test_plot_growth_rate.py
+++ tests/test_plot_growth_rate.py
@@ -1,18 +1,26 @@
 from population_trend import Plotter_Growth_Rate
 import matplotlib as plt
+import numpy as np
 
 
 def test_plotter_growth_rate():
-    lambdas_dict = {"intervals": [1, 2, 3]}
-    lambdas_dict_2 = {"intervals": [2, 3, 8]}
+    lambdas_dict = {"intervals": [1, 2, 3.5]}
+    lambdas_dict_2 = {"intervals": [1.9, 3, 7.5]}
     plotter = Plotter_Growth_Rate(lambdas_dict, lambdas_dict_2)
     obtained = plotter.plot_error_bars()
-    assert plotter.interval == [[1, 2, 3], [2, 3, 8]]
+
+    x_positions = plotter.error_bar_container.lines[0].get_data()[0]
+    y_positions = plotter.error_bar_container.lines[0].get_data()[1]
+    np.testing.assert_array_equal(x_positions, [1, 2])
+    np.testing.assert_array_equal(y_positions, [2, 3])
+    obtained_segments = plotter.error_bar_container.lines[2][0].properties()["segments"]
+    print(obtained_segments[0][0][1])
+    assert obtained_segments[0][0][1] == lambdas_dict["intervals"][0]
+    print(plotter.error_bar_container.lines[2][0].properties()["segments"][1])
     assert isinstance(obtained, plt.axes._axes.Axes)
     obtained_y_label = obtained.get_ylabel()
     expected_y_label = "Growth Rate"
     assert obtained_y_label == expected_y_label
-    plt.pyplot.savefig("tests/data/borrame.png")
     assert obtained.get_xticklabels()[0].get_text() == "California"
     assert obtained.get_xticklabels()[1].get_text() == "Pacific"
     assert obtained.get_xticklabels()[1].get_fontsize() == 20.0
