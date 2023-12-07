cf8f3b19a960bddde39eb0be3fe73a5c451ae0aa
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-07 19:06:50 +0000

    🛑🧪 Fail tests

--- tests/test_plot_growth_rate.py
+++ tests/test_plot_growth_rate.py
@@ -1,8 +1,10 @@
 from population_trend import Plotter_Growth_Rate
+import matplotlib as plt
 
 
 def test_plotter_growth_rate():
     lambdas_dict = {"intervals": [1, 2, 3]}
     plotter = Plotter_Growth_Rate(lambdas_dict, lambdas_dict)
-    plotter.plot_error_bars()
+    obtained = plotter.plot_error_bars()
     assert plotter.interval == [[1, 2, 3], [1, 2, 3]]
+    assert isinstance(obtained, plt.axes._axes.Axes)
