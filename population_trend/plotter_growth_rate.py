3bfaa6efec2654b5ff84b38b01227083338c1b59
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-07 19:02:08 +0000

    ✅ Pass tests

--- population_trend/plotter_growth_rate.py
+++ population_trend/plotter_growth_rate.py
@@ -1,6 +1,6 @@
 class Plotter_Growth_Rate:
-    def __init__(self, lambdas_dict):
-        self.interval = lambdas_dict["intervals"]
+    def __init__(self, lambdas_dict, lambdas_dict_2):
+        self.interval = [lambdas_dict["intervals"], lambdas_dict_2["intervals"]]
 
     def plot_error_bars(self):
         pass
