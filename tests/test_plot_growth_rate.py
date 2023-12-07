ef6b267792130295fa759622f78db399a0d874f6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-07 20:44:05 +0000

    🛑🧪 Fail tests

--- tests/test_plot_growth_rate.py
+++ tests/test_plot_growth_rate.py
@@ -4,9 +4,10 @@ import matplotlib as plt
 
 def test_plotter_growth_rate():
     lambdas_dict = {"intervals": [1, 2, 3]}
-    plotter = Plotter_Growth_Rate(lambdas_dict, lambdas_dict)
+    lambdas_dict_2 = {"intervals": [2, 3, 8]}
+    plotter = Plotter_Growth_Rate(lambdas_dict, lambdas_dict_2)
     obtained = plotter.plot_error_bars()
-    assert plotter.interval == [[1, 2, 3], [1, 2, 3]]
+    assert plotter.interval == [[1, 2, 3], [2, 3, 8]]
     assert isinstance(obtained, plt.axes._axes.Axes)
     obtained_y_label = obtained.get_ylabel()
     expected_y_label = "Growth Rate"
@@ -15,3 +16,7 @@ def test_plotter_growth_rate():
     assert obtained.get_xticklabels()[0].get_text() == "California"
     assert obtained.get_xticklabels()[1].get_text() == "Pacific"
     assert obtained.get_xticklabels()[1].get_fontsize() == 20.0
+
+    obtained_xlim = plt.pyplot.xlim()
+    expected_xlim = (0.5, 2.5)
+    assert obtained_xlim == expected_xlim
