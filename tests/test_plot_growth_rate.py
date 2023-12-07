98b8acd8bf262ce1312043c37c5af471e4f6ede4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-07 18:41:30 +0000

    🛑🧪 Fail tests

--- tests/test_plot_growth_rate.py
+++ tests/test_plot_growth_rate.py
@@ -2,4 +2,5 @@ from population_trend import Plotter_Growth_Rate
 
 
 def test_plotter_growth_rate():
-    Plotter_Growth_Rate()
+    lambdas_dict = {"intervals": [1, 2, 3]}
+    plotter = Plotter_Growth_Rate(lambdas_dict)
