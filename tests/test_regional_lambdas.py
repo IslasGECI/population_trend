24d0c7005eb09fbcaa5b40dd98c8ee45519a5428
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-01 21:01:01 +0000

    🛑🧪 Fail tests

--- tests/test_regional_lambdas.py
+++ tests/test_regional_lambdas.py
@@ -70,3 +70,11 @@ def test_Calculator_Regional_Lambdas_Intervals_hypotesis_test_statement_latex():
         f"La población está decreciendo. $H_0: \lambda > 1$, $\alpha > p =$ 0.0"
     )
     assert obtained_statement_latex == expected_statement_latex
+
+    regional_lambdas = np.array([1, 2, 3, 2, 2, 2, 4])
+    calculator = Calculator_Regional_Lambdas_Intervals(regional_lambdas)
+    obtained_statement_latex = calculator.hypotesis_test_statement_latex
+    expected_statement_latex = (
+        f"La población está creciendo. $H_0: \lambda < 1$, $\alpha > p =$ 0.0"
+    )
+    assert obtained_statement_latex == expected_statement_latex
