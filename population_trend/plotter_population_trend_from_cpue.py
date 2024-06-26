a762342424ce6b38e1fd86634335e39266dfed91
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-06-26 18:22:45 +0000

    ✅ Pass tests

--- population_trend/plotter_population_trend_from_cpue.py
+++ population_trend/plotter_population_trend_from_cpue.py
@@ -7,3 +7,11 @@ class Plotter_Population_Trend_Model_From_CPUE(Plotter_Population_Trend_Model):
     def set_labels(self):
         plt.ylabel("Max CPUE", size=20)
         plt.xlabel("Seasons", size=20)
+
+    def plot_data(self):
+        plt.plot(
+            self.plot_seasons,
+            self.data[self.interest_variable],
+            "-Dk",
+            label="Maximum CPUE",
+        )
