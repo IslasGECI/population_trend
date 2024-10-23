053bf47e725a493b24dfc69e5bcaaf0c525f3776
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-23 17:20:01 +0000

    ♻️ Refactor

--- population_trend/plotter_growth_rate.py
+++ population_trend/plotter_growth_rate.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def Plotter_Growth_Rate(lambdas_dict, lambdas_dict_2):
-    return xxPlotter_Growth_Rate(lambdas_dict, lambdas_dict_2)
+    regional_names = ["Gulf of California", "Pacific"]
+    return xxPlotter_Growth_Rate(lambdas_dict, lambdas_dict_2, regional_names)
 
 
 class xxPlotter_Growth_Rate:
-    def __init__(self, lambdas_dict, lambdas_dict_2):
+    def __init__(self, lambdas_dict, lambdas_dict_2, regional_names):
         self.interval = [lambdas_dict["intervals"], lambdas_dict_2["intervals"]]
+        self.regional_names = regional_names
 
     def plot_error_bars(self):
         _, ax = gp.geci_plot()
@@ -33,6 +35,6 @@ class xxPlotter_Growth_Rate:
             1, xmin=xlimits[0], xmax=xlimits[1], linestyles="dashed", colors="green"
         )
         plt.ylabel("Growth Rate", fontsize=20)
-        plt.xticks(xticks_position, ["Gulf of California", "Pacific"], fontsize=20)
+        plt.xticks(xticks_position, self.regional_names, fontsize=20)
         plt.xlim(xlimits)
         return ax
