a62ad8ec857022623917382ae75e29368000527c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-01-27 22:38:42 +0000

    ♻️ Refactor

--- population_trend/population_growth_model.py
+++ population_trend/population_growth_model.py
@@ -56,7 +56,7 @@ class Plotter_Population_Trend_Model:
         self.fig, self.ax = geci_plot()
         self.data = data
         self.tick_mode = tick_mode
-        self.ticks_text = normalize_seasons(self.data, tick_step=1)
+        self.ticks_text = self.data.Temporada.values.astype(int)
         self.ticks_positions = ticks_positions_array(self.data)
         self.plot_seasons = self.data.index.values + 1
         self.plot_domain = np.linspace(self.ticks_positions.min(), self.ticks_positions.max(), 100)
