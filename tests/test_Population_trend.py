e35a819155db27c0c033c1f932260b7d9933869c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-06 21:44:37 +0000

    🛑🧪 Fail tests

--- tests/test_Population_trend.py
+++ tests/test_Population_trend.py
@@ -91,6 +91,10 @@ class Tests_Plotter_Population_Trend_Model:
         obtained_labels = [Plotter.ax.get_xlabel(), Plotter.ax.get_ylabel()]
         expected_labels = ["Seasons", "Number of breeding pairs"]
         assert obtained_labels == expected_labels
+        Plotter.set_labels("spanish")
+        obtained_labels = [Plotter.ax.get_xlabel(), Plotter.ax.get_ylabel()]
+        expected_labels = ["Temporadas", "Número de parejas reproductivas"]
+        assert obtained_labels == expected_labels
 
     def tests_savefig(self):
         islet = "morro"
