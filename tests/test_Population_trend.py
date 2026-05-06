bb1087507293d952deb0efce7c51e82569c47e5d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-06 21:43:25 +0000

    ♻️ Refactor add missing tests

--- tests/test_Population_trend.py
+++ tests/test_Population_trend.py
@@ -63,8 +63,8 @@ Sparse_Plotter = Plotter_Population_Trend_Model(more_cormorant_data, pop_model, 
 Plotter = Plotter_Population_Trend_Model(cormorant_data_for_plotter, pop_model, tick_mode="full")
 
 
-class Tests_Plotter_Population_Trend_Model:
 
+class Tests_Plotter_Population_Trend_Model:
     def tests_init_(self):
         fig, ax = geci_plot()
         assert type(fig) == type(Plotter.fig)  # noqa
@@ -85,6 +85,12 @@ class Tests_Plotter_Population_Trend_Model:
         assert obtained[0] == expected_first_point
         expected_last_point = 4.05
         assert obtained[-1] == expected_last_point
+
+    def tests_set_labels(self):
+        Plotter.set_labels()
+        obtained_labels = [Plotter.ax.get_xlabel(), Plotter.ax.get_ylabel()]
+        expected_labels = ["Seasons", "Number of breeding pairs"]
+        assert obtained_labels == expected_labels
 
     def tests_savefig(self):
         islet = "morro"
