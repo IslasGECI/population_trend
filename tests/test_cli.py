594cf944629897c178dbd2b7f4b4254a2c00f3b5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-12 21:32:02 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -258,3 +258,10 @@ def test_render_population_trend_new_command_works():
     assert result.exit_code == 0
     gtt.assert_exist(output_figure_render)
     gtt.if_exist_remove(output_figure_render)
+
+
+def test_app_version():
+    result = runner.invoke(app, ["help"])
+    assert result.exit_code == 0
+    expected_version = "5.12.0"
+    assert expected_version in result.stdout
